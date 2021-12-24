8c8fc99b82ec78d0f0812b31bfe783826cdc07d4
monk <l312940@gmail.com>
2021-12-24 17:54:22 +0800

    feat: update mergesort_inplace method

---
+++ src/sort/merge_sort_Arr_inplace.java
@@ -0,0 +1,61 @@
+package sort;
+
+public class merge_sort_Arr_inplace {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public int[] mergesort(int[] arr) {
+
+		// split
+		split(arr, 0, arr.length - 1);
+
+		return arr;
+	}
+
+	private void split(int[] arr, int s, int e) {
+
+		if (s >= e) {
+			return;
+		}
+		// middle
+		int middle = s + (e - s) / 2;
+
+		// split
+		split(arr, s, middle);
+		split(arr, middle + 1, e);
+
+		// merge
+		merge(arr, s, middle, e);
+
+	}
+
+	private void merge(int[] arr, int s, int middle, int e) {
+
+		int st2 = middle + 1;
+		if (arr[middle] <= arr[st2]) {
+			return;
+		}
+
+
+		while (s <= middle && st2 <= e) {  //»İequal to compare the last one
+
+			if (arr[s] > arr[st2]) {
+				int val = arr[st2];
+				int idx = st2;
+				while (s < idx) {
+					arr[idx] = arr[--idx];
+				}
+				arr[s] = val;
+
+				middle++;
+				st2++;
+			}
+			s++;
+		}
+
+	}
+
+}
