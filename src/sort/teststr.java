8576027d4a37cff7c979674c310bcdeee6bb4ab6
monk <l312940@gmail.com>
2022-01-17 11:54:17 +0800

    comit

---
+++ src/sort/teststr.java
@@ -0,0 +1,18 @@
+package sort;
+
+import java.io.File;
+
+public class teststr {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			
+		File test = new File("d://jdiscl");
+		
+		System.out.println(test.exists());
+		
+		
+		
+	}
+
+}
