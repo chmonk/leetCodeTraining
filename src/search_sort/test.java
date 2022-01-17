ea768b04cac9688eeb3eb365e888a854ed220d81
monk <l312940@gmail.com>
2022-01-17 10:51:33 +0800

    add test.java

---
+++ src/search_sort/test.java
@@ -0,0 +1,31 @@
+package search_sort;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class test {
+
+	public static void main(String[] args) throws ParseException {
+		// TODO Auto-generated method stub
+
+		
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+		
+		
+		 sdf.setLenient(false);
+		 
+		 
+		 System.out.println("1999-02-18".replaceAll("-|/", ""));
+		 
+		 Date dgd1 = sdf.parse("1999-02-18".replace("-|/", ""));
+		Date dgd = sdf.parse("1999-13-1".replace("-|/", ""));
+ 
+        System.out.println(dgd1);
+        System.out.println(dgd);
+		
+		
+	}
+
+}
